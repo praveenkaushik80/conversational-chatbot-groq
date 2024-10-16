096fd1cfa1d9a7daa7474a269a07d19f9aea43c7
Praveen Kaushik <praveenkaushik80@icloud.com>
2024-10-16 16:23:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,56 +33,73 @@ def main():
     st.write("Hello! I'm your friendly Groq chatbot. I can help answer your questions, provide information, or just chat. I'm also super fast! Let's start our conversation!")
 
     # Add customization options to the sidebar
-    st.sidebar.title('Customization')
-    system_prompt = st.sidebar.text_input("System prompt:")
-    model = st.sidebar.selectbox(
-        'Choose a model',
-        ['llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it']
-    )
-    conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
-
-    memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
-
-    user_question = st.text_input("Ask a question:")
-
-    # session state variable
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history=[]
-    else:
-        for message in st.session_state.chat_history:
-            memory.save_context(
-                {'input':message['human']},
-                {'output':message['AI']}
-                )
-
-
-    # Initialize Groq Langchain chat object and conversation
-    groq_chat = ChatGroq(
-            groq_api_key=groq_api_key, 
-            model_name=model
-    )
-
-
+    with st.sidebar:
+        st.sidebar.title('Customization')
+        model = st.sidebar.selectbox(
+            'Choose a model',
+            # ['llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it']
+            [
+                "llama3-8b-8192",
+                "llama3-70b-8192",
+                "llama-3.1-8b-instant",
+                "llama-3.1-70b-versatile",
+                "llama-3.2-1b-preview",
+                "llama-3.2-3b-preview",
+                "llama-3.2-11b-text-preview",
+                "llama-3.2-90b-text-preview",
+                "mixtral-8x7b-32768",
+                "gemma-7b-it",
+                "gemma2-9b-it"
+            ]
+        )
+        groq_api_key = st.text_input("Groq API Key", type="password")
+        if not groq_api_key:
+            st.info("Please add your Groq API key to continue.", icon="🗝️")
+        else:
+            system_prompt = st.sidebar.text_input("System prompt:")
+            conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value = 5)
+        
+            memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
+        
+            user_question = st.text_input("Ask a question:")
+        
+            # session state variable
+            if 'chat_history' not in st.session_state:
+                st.session_state.chat_history=[]
+            else:
+                for message in st.session_state.chat_history:
+                    memory.save_context(
+                        {'input':message['human']},
+                        {'output':message['AI']}
+                        )
+        
+        
+            # Initialize Groq Langchain chat object and conversation
+            groq_chat = ChatGroq(
+                    groq_api_key=groq_api_key, 
+                    model_name=model
+            )
+    
     # If the user has asked a question,
     if user_question:
-
+    
         # Construct a chat prompt template using various components
         prompt = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(
                     content=system_prompt
                 ),  # This is the persistent system prompt that is always included at the start of the chat.
-
+    
                 MessagesPlaceholder(
                     variable_name="chat_history"
                 ),  # This placeholder will be replaced by the actual chat history during the conversation. It helps in maintaining context.
-
+    
                 HumanMessagePromptTemplate.from_template(
                     "{human_input}"
                 ),  # This template is where the user's current input will be injected into the prompt.
             ]
         )
-
+    
         # Create a conversation chain using the LangChain LLM (Language Learning Model)
         conversation = LLMChain(
             llm=groq_chat,  # The Groq LangChain chat object initialized earlier.
